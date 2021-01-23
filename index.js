f76d90c01a7a7758c85c814d54ee39f702423cb3
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2021-01-23 23:35:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+let multiplier = () => {
+  let x = 1000;
+  let y = 55;
+  let mult = x*y;
+  return mult * 99;
+}
+
+let adder = (x,y) => x+y;
+
+let divider = (x,y) => {
+  if(y === 0)
+  x/(y+1);
+  else {
+    x/y;
+  }
+}
